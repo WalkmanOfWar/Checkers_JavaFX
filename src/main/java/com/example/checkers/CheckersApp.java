f672f01f3440029933a2c8c2e06cb8933beba96b
acer <wonverin@gmail.com>
2022-05-07 19:01:05 +0200

    Added standard Piece in fxml

--- src/main/java/com/example/checkers/CheckersApp.java
+++ src/main/java/com/example/checkers/CheckersApp.java
@@ -22,6 +22,7 @@ public class CheckersApp extends Application {
     private Parent createContent(){
         Pane root = new Pane();
         root.setPrefSize(WIDTH*TILE_SIZE, HEIGHT*TILE_SIZE);
+        root.getChildren().addAll(tileGroup,pieceGroup);
         for (int y=0;y<HEIGHT;y++){
             for (int x = 0;x<WIDTH;x++){
                 Tile tile = new Tile((x+y)%2==0,x,y);
--- src/main/java/com/example/checkers/Piece.java
+++ src/main/java/com/example/checkers/Piece.java
@@ -1,7 +1,27 @@
 package com.example.checkers;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Ellipse;
+
+import java.io.IOException;
+
+import static com.example.checkers.CheckersApp.TILE_SIZE;
 
 public class Piece extends StackPane {
-    private Piece piece;
+    private PieceType type;
+    private  PieceType getType(){
+        return type;
+    }
+    public Piece(PieceType type, int x, int y) throws IOException {
+        this.type = type;
+        relocate(x*TILE_SIZE, y*TILE_SIZE);
+        Ellipse bg;
+        if(type == PieceType.RED)
+            bg = FXMLLoader.load(getClass().getResource("com/example/checkers/RedPiece.fxml"));
+        else
+            bg = FXMLLoader.load(getClass().getResource("com/example/checkers/WhitePiece.fxml"));
+
+        getChildren().add(bg);
+    }
 }
